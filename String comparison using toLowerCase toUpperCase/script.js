eca9f31af46a0b6ac153d24421ef4fe8fa5eda63
Mohammodrubel <74967048+mohammodrubel@users.noreply.github.com>
2021-08-19 11:18:43 +0600

    Add files via upload

---
+++ String comparison using toLowerCase toUpperCase/script.js
@@ -0,0 +1,17 @@
+const fastName  = 'blackPink'
+const lastName = 'blacKpinK'
+
+    if (fastName == lastName){
+        console.log('this is equal! ')
+    }else{
+        console.log('this is not a equal')
+    }
+
+const fastname  = 'blackPink'
+const lastname = 'blacKpinK'
+    
+    if (fastname.toLocaleLowerCase == lastname.toLocaleLowerCase){
+        console.log('this is equal! ')
+    }else{
+        console.log('this is not a equal')
+    }
